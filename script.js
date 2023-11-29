a6a2677fced1824c4e1c0c2eaccbb40a7c8ea12b
Simon Rehn <quesadilla.guerilla@protonmail.com>
2023-11-29 10:23:41 +0100

    pokemon

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-let foo = document.querySelector(".lol");
-alert(foo);
+const API_URL = "https://pokeapi.co/api/v2/pokemon";
+const root = document.getElementById("root");
+const form = document.getElementById("addPokemonForm");
+
+form.addEventListener("submit", (event) => {
+  console.log("Hallo");
+  const pokemonName = document.getElementById("newPokemonName").value.toLowerCase();
+  event.preventDefault();
+  fetch(`${API_URL}/${pokemonName}`)
+  .then((response) => response.json())
+  .then((newPokemon) => {
+    console.log(newPokemon)
+    const div = document.createElement("div");
+    const image = document.createElement("img");
+    const name = document.createElement("h1");
+
+    div.className = "card";
+    image.src = newPokemon.sprites.other.dream_world.front_default;
+    name.textContent = newPokemon.name;
+
+    div.appendChild(name);
+    div.appendChild(image);
+    root.appendChild(div);
+  });
+});
