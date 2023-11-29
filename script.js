baab0441c445880150774687548ff65de91ce29d
OskarBjork <oskar.bjork@skola.taby.se>
2023-11-29 10:51:21 +0100

    började på UIn

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 let foo = document.querySelector(".lol");
-alert(foo);
+// alert(foo);
+
+const dropDiv = document.querySelector(".dropdown");
+const dropBtn = document.querySelector(".dropbtn");
+const dropDownContentDiv = document.querySelector(".dropdown-content");
+const dropDownContents = dropDownContentDiv.childNodes;
+
+// console.log(dropDownContents);
+// console.log(dropBtn);
+
+function changeDropDownContents(dropDownContents) {
+  dropDownContents.forEach(function (link) {
+    console.log(link);
+  });
+}
+
+changeDropDownContents(dropDownContents);
+
+// Event Listeners
+
+dropBtn.addEventListener("click", function () {
+  dropDownContentDiv.classList.toggle("hidden");
+  // dropDownContentDiv.style.display = "block";
+  // console.log(dropDownContentDiv.classList);
+});
+
+// dropBtn.addEventListener("");
+
+for (let i = 0; i < dropDownContents.length; i++) {
+  dropDownContents[i].addEventListener("mouseover", function () {
+    dropDownContents[i].classList.add("hovered");
+  });
+  dropDownContents[i].addEventListener("mouseout", function () {
+    dropDownContents[i].classList.remove("hovered");
+  });
+}
