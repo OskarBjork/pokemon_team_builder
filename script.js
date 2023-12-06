024ea4203e42295f6dcaa372f1342f8d6aaf6831
OskarBjork <oskar.bjork@skola.taby.se>
2023-12-06 17:30:38 +0100

    laddar nu pokemon utifrån generation dock inte i rätt ordning

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ const GENERATION_URL = "https://pokeapi.co/api/v2/generation";
 const dropDiv = document.querySelector(".dropdown");
 const dropBtn = document.querySelector(".dropbtn");
 const dropDownContentDiv = document.querySelector(".dropdown-content");
+const pokemonListDiv = document.querySelector(".pokemon-list-box");
 
 // Event Listeners
 
 dropBtn.addEventListener("click", function () {
+  console.log("click");
+  console.log(dropDownContentDiv.classList);
   dropDownContentDiv.classList.toggle("hidden");
   // dropDownContentDiv.style.display = "block";
   // console.log(dropDownContentDiv.classList);
@@ -30,11 +33,11 @@ async function getGenerations() {
 
   try {
     const response = await fetch(GENERATION_URL);
-    const gen_data = await response.json();
-    for (let i = 0; i < gen_data.count; ++i) {
+    const genData = await response.json();
+    for (let i = 0; i < genData.count; ++i) {
       generations.push({
-        name: gen_data.results[i].name,
-        url: gen_data.results[i].url,
+        name: genData.results[i].name,
+        url: genData.results[i].url,
       });
     }
   } catch (error) {
@@ -48,16 +51,79 @@ async function loadGenerations() {
   const generations = await getGenerations();
   generations.forEach(function (generation) {
     const p = document.createElement("p");
+    p.className = "pokemon-game-box";
     p.textContent = generation.name;
-    p.addEventListener("mouseover", function () {
-      p.classList.add("hovered");
-    });
-    p.addEventListener("mouseout", function () {
-      p.classList.remove("hovered");
-    });
-    p.classList.add("pokemon-game-box");
+    addParagraphEventListeners(p, generation);
     dropDownContentDiv.appendChild(p);
   });
+}
+
+function addParagraphEventListeners(p, generation) {
+  p.addEventListener("mouseover", function () {
+    p.classList.add("hovered");
+  });
+  p.addEventListener("mouseout", function () {
+    p.classList.remove("hovered");
+  });
+  p.addEventListener("click", function () {
+    console.log("click");
+    loadGenerationPokemon(generation);
+  });
+}
+
+async function loadGenerationPokemon(generation) {
+  // console.log("loadGenerationPokemon");
+  const pokemon = await getGenerationPokemon(generation);
+  pokemon.forEach(async function (pokemon) {
+    // console.log(pokemon.name);
+    const newPokemon = new Pokemon(pokemon.name);
+    const pokemonSpriteUrl = await newPokemon.getSpriteUrl();
+    const pokemonHp = await newPokemon.getHp();
+    let pokemonDiv = createPokemonDiv(newPokemon, pokemonSpriteUrl, pokemonHp);
+    pokemonListDiv.appendChild(pokemonDiv);
+  });
+}
+
+function createPokemonDiv(pokemon, pokemonSpriteUrl, pokemonHp) {
+  const div = document.createElement("div");
+  const image = document.createElement("img");
+  const name = document.createElement("p");
+  const hp = document.createElement("p");
+
+  div.className = "pokemon-preview";
+  // console.log(pokemon);
+  image.src = pokemonSpriteUrl;
+
+  name.textContent = pokemon.name;
+  name.className = "pokemon-info";
+
+  (hp.textContent = "HP: "), pokemonHp;
+  hp.className = "pokemon-info";
+
+  div.appendChild(image);
+  div.appendChild(name);
+  div.appendChild(hp);
+
+  return div;
+}
+
+async function getGenerationPokemon(generation) {
+  const generationUrl = generation.url;
+  let pokemon = [];
+  try {
+    const response = await fetch(generationUrl);
+    const genData = await response.json();
+    for (let i = 0; i < genData.pokemon_species.length; ++i) {
+      pokemon.push({
+        name: genData.pokemon_species[i].name,
+        url: genData.pokemon_species[i].url,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return pokemon;
 }
 
 function getEvolutionChainUrl(pokemonName) {
